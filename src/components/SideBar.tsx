53de8ba9f18b9b87031cd8be6f8c86196eee522a
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-03-20 16:49:43 -0300

    feat: componentizing

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+import {GenreResponseProps} from '../App'
+
+interface SideBarProps{
+  fn: (id:number) => void,
+  selectedGenreId:number,
+  genres:GenreResponseProps[],
+}
+
+export function SideBar({fn, genres, selectedGenreId}:SideBarProps) {
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => fn(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
